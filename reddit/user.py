ad6e8ec8209889ab4c27905696dfda5bb1fd4a54
itgsod-Gustav-Wolgast <gustav.wolgast@elev.it-gymnasiet.se>
2015-05-06 11:41:49 +0200

    Börjat på users

--- reddit/user.py
+++ reddit/user.py
@@ -4,6 +4,9 @@ import reddit
 
 url_me = "https://oauth.reddit.com/api/v1/me"
 url_about = "https://oauth.reddit.com/api/v1/user/{username}/about"
+url_submit = "https://oauth.reddit.com/api/submit"
+url_comment = "https://oauth.reddit.com/api/comment"
+
 
 class User(object):
 
